55dad058ccc6fa84f85aac3070d348cf793eb890
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2020-04-23 09:42:58 +0530

    Added: get_detections method for inference

--- ssd/models/ssd_model.py
+++ ssd/models/ssd_model.py
@@ -22,17 +22,50 @@ class SSDModel(tf.keras.Model):
         self._decode_predictions = DecodePredictions(config)
         self._network = self._build_network()
 
-    @tf.function
-    def call(self, x, training=False):
+    def compile(self, loss_fn, optimizer, **kwargs):
+        super(SSDModel, self).compile(**kwargs)
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+
+    def call(self, x, training):
         return self._network(x, training=training)
 
     @tf.function
     def train_step(self, data):
-        pass
+        images, y_true = data[0], data[1]
+
+        with tf.GradientTape() as tape:
+            y_pred = self(images, training=True)
+            loss = self.loss_fn(y_true, y_pred)
+            cls_loss = loss[0]
+            loc_loss = loss[1]
+            total_loss = tf.reduce_sum(loss, axis=0)
+        gradients = tape.gradient(total_loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+
+        loss_dict = {
+            'cls_loss': cls_loss,
+            'loc_loss': loc_loss,
+            'loss': total_loss
+        }
+        return loss_dict
 
     @tf.function
     def test_step(self, data):
-        pass
+        images, y_true = data[0], data[1]
+
+        y_pred = self.call(images, training=False)
+        loss = self.loss_fn(y_true, y_pred)
+        cls_loss = loss[0]
+        loc_loss = loss[1]
+        total_loss = tf.reduce_sum(loss, axis=0)
+        
+        loss_dict = {
+            'cls_loss': cls_loss,
+            'loc_loss': loc_loss,
+            'loss': total_loss,
+        }
+        return loss_dict
 
     @tf.function
     def predict_step(self, data):
@@ -44,6 +77,10 @@ class SSDModel(tf.keras.Model):
             'cls_ids': cls_ids,
             'scores': scores
         }
+    
+    @tf.function
+    def get_detections(self, images):
+        return self.predict_step((images,))
 
     def _build_network(self):
         def _conv_block(tensor, filters, k_size, stride, padding, use_bn=False):
@@ -63,7 +100,7 @@ class SSDModel(tf.keras.Model):
             output_shape = [0, 4 + num_classes]
             for i in range(self._num_feature_maps):
                 grid_locations = self._feature_shapes[i][1] * self._feature_shapes[i][0]
-                num_default_boxes = len(self._aspect_ratios[i]) + 1  # 1 addition default for 1:1 ratio
+                num_default_boxes = len(self._aspect_ratios[i]) + 1  # 1 addition default box for 1:1 ratio
                 output_shape[0] += grid_locations * num_default_boxes
             return output_shape
 
