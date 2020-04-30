b79eedd519107ff59ebb9755fb252aca4a47e70a
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2020-04-30 07:42:18 +0530

    Implemented: relative_to_absolute; absolute_to_relative; swap_xy

--- ssd/common/box_utils.py
+++ ssd/common/box_utils.py
@@ -25,9 +25,10 @@ def rescale_boxes(boxes, original_dims, new_dims):
     return tf.stack([
         boxes[..., 0] * scale[1],
         boxes[..., 1] * scale[0],
-        boxes[..., 2] * scale[1],
+        boxes[..., 2] * scale[1], 
         boxes[..., 3] * scale[0]
     ], axis=-1)
+
 
 def relative_to_absolute(boxes, image_dims):
     boxes = tf.cast(boxes, dtype=tf.float32)
@@ -39,6 +40,7 @@ def relative_to_absolute(boxes, image_dims):
         boxes[..., 3] * image_dims[0]
     ], axis=-1)
 
+
 def absolute_to_relative(boxes, image_dims):
     boxes = tf.cast(boxes, dtype=tf.float32)
     image_dims = tf.cast(image_dims, dtype=tf.float32)
@@ -47,4 +49,14 @@ def absolute_to_relative(boxes, image_dims):
         boxes[..., 1] / image_dims[0],
         boxes[..., 2] / image_dims[1],
         boxes[..., 3] / image_dims[0]
+    ], axis=-1)
+
+
+def swap_xy(boxes):
+    boxes = tf.cast(boxes, dtype=tf.float32)
+    return tf.stack([
+        boxes[:, 1],
+        boxes[:, 0],
+        boxes[:, 3],
+        boxes[:, 2],
     ], axis=-1)
