ba9fe966d30c91372063bdc06e33c32d68760191
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2020-04-30 07:43:46 +0530

    Implemented: Parser; CocoParser classes

---
+++ ssd/data/parsers/base_parser.py
@@ -0,0 +1,50 @@
+from abc import ABC, abstractmethod
+
+
+class Parser(ABC):
+    def __init__(self, download_path):
+        self._download_path = download_path
+
+        {
+            'image': None,
+            'label': {
+                'boxes': None,
+                'classes': None
+            }
+        }
+
+        self._data = {
+            'train': [],
+            'val': [],
+            'test': []
+        }
+        self._classes = []
+        self._class_name_to_class_id = {}
+        self._class_id_to_class_name = {}
+        self._class_id_to_train_id = {}
+        self._train_id_to_class_id = {}
+
+    def get_class_id(self, train_id=None, class_name=None):
+        if train_id is not None:
+            return self._train_id_to_class_id[train_id]
+        return self._class_name_to_class_id[class_name]
+
+    def get_train_id(self, class_id):
+        return self._class_id_to_train_id[class_id]
+
+    def get_class_name(self, train_id=None, class_id=None):
+        if train_id is not None:
+            class_id = self._train_id_to_class_id[train_id]
+        return self._class_id_to_class_name[class_id]
+
+    @abstractmethod
+    def _build_dataset(self):
+        pass
+
+    @property
+    def dataset(self):
+        return self._data
+
+    @property
+    def classes(self):
+        return self._classes
---
+++ ssd/data/parsers/coco_parser.py
@@ -0,0 +1,64 @@
+import os
+
+import numpy as np
+from pycocotools.coco import COCO
+
+from ssd.data.parsers.base_parser import Parser
+
+
+class CocoParser(Parser):
+    def __init__(self, download_path, only_mappings=False):
+        super(CocoParser, self).__init__(download_path)
+        self._train_annotations_path = os.path.join(download_path, 'annotations/instances_train2017.json')
+        self._val_annotations_path = os.path.join(download_path, 'annotations/instances_val2017.json')
+        self._only_mappings = only_mappings
+        self._build_dataset()
+
+    def _build_dataset(self):
+        def _convert_box_format(boxes):
+            boxes = np.array(boxes)
+            return np.concatenate([
+                boxes[:, :2],
+                boxes[:, :2] + boxes[:, 2:]
+            ], axis=-1)
+
+        def _build(annptations_path, split_name):
+            print('Building {} data'.format(split_name))
+            coco = COCO(annptations_path)
+            if self._class_id_to_class_name == {}:
+                self._class_id_to_class_name = {cat_dict['id']: cat_dict['name'] for _, cat_dict in coco.cats.items()}
+            if self._class_name_to_class_id == {}:
+                self._class_name_to_class_id = {cat_dict['name']: cat_dict['id'] for _, cat_dict in coco.cats.items()}
+
+            self._classes = sorted(self._class_name_to_class_id.keys())
+
+            if self._class_id_to_train_id == {}:
+                self._class_id_to_train_id = {
+                    self._class_name_to_class_id[class_name]: idx for idx, class_name in enumerate(self._classes)}
+            if self._train_id_to_class_id == {}:
+                self._train_id_to_class_id = {
+                    idx: self._class_name_to_class_id[class_name] for idx, class_name in enumerate(self._classes)}
+
+            if self._only_mappings:
+                return
+            
+            for image_id, annotation in coco.imgToAnns.items():
+                image_path = coco.imgs[image_id]['file_name']
+                boxes = []
+                classes = []
+                for obj in annotation:
+
+                    boxes.append(obj['bbox'])
+                    classes.append(self.get_train_id(obj['category_id']))
+
+                sample = {
+                    'image': image_path,
+                    'label': {
+                        'boxes': _convert_box_format(boxes),
+                        'classes': classes
+                    }
+                }
+                self._data[split_name].append(sample)
+
+        _build(self._train_annotations_path, 'train')
+        _build(self._val_annotations_path, 'val')
