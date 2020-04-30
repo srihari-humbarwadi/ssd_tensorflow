11c20d08adae34eb9677e69e6f68ec3a7052939d
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2020-04-30 07:43:12 +0530

    Implemented: Random cropping

--- ssd/data/dataset_builder.py
+++ ssd/data/dataset_builder.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-from ssd.common.box_utils import convert_to_xywh, rescale_boxes
+from ssd.common.box_utils import convert_to_xywh, rescale_boxes, absolute_to_relative, swap_xy, relative_to_absolute
 from ssd.common.label_encoder import LabelEncoder
 
 
@@ -26,6 +26,9 @@ class DatasetBuilder:
         self._contrast_upper = config['contrast_upper']
         self._saturation_lower = config['saturation_lower']
         self._saturation_upper = config['saturation_upper']
+        self._min_obj_covered = config['min_obj_covered']
+        self._area_range = config['area_range']
+        self._aspect_ratio_range = config['aspect_ratio_range']
         self._cache_dataset_in_memory = config['cache_dataset_in_memory']
         self._build_tfrecord_dataset()
 
@@ -50,14 +53,54 @@ class DatasetBuilder:
         image = tf.image.random_contrast(image, self._saturation_lower, self._saturation_upper)
         return tf.clip_by_value(image, 0.0, 1)
 
-    def _random_patch_fn(self, image, boxes):
-        pass
+    def _filter_and_adjust_labels(self, crop_box, boxes, classes):
+        boxes = tf.cast(boxes, dtype=tf.float32)
+        crop_box = tf.cast(crop_box, dtype=tf.float32)
 
+        offsets = tf.concat([
+            crop_box[:, :2] - boxes[:, 2:], 
+            boxes[:, :2] - crop_box[:, 2:],
+        ], axis=-1)
 
-    def _augment_data(self, image, boxes):
+        crop_box_width = crop_box[:, 2] - crop_box[:, 0]
+        crop_box_height = crop_box[:, 3] - crop_box[:, 1]
+
+        adjusted_boxes = tf.stack([
+            tf.clip_by_value(boxes[:, 0] - crop_box[:, 0], 0, crop_box_width),
+            tf.clip_by_value(boxes[:, 1] - crop_box[:, 1], 0, crop_box_height),
+            tf.clip_by_value(boxes[:, 2] - crop_box[:, 0], 0, crop_box_width),
+            tf.clip_by_value(boxes[:, 3] - crop_box[:, 1], 0, crop_box_height)
+        ], axis=-1)
+
+        idx = tf.where(tf.logical_not(tf.reduce_all(offsets >= 0, axis=-1)))[:, 0]
+        return tf.gather(adjusted_boxes, idx), tf.gather(classes, idx)
+
+    def _random_patch_fn(self, image, boxes, classes):
+        boxes = absolute_to_relative(boxes, tf.shape(image))
+
+        start, size, crop_box = tf.python.image.sample_distorted_bounding_box_v2(
+            image_size=tf.shape(image),
+            bounding_boxes=tf.expand_dims(swap_xy(boxes), axis=0),
+            min_object_covered=self._min_obj_covered,
+            area_range=self._area_range,
+            aspect_ratio_range=self._aspect_ratio_range)
+
+        crop_box = relative_to_absolute(swap_xy(crop_box[0]), tf.shape(image))
+
+        cropped_image = tf.slice(image, start, size)
+        cropped_image.set_shape([None, None, 3])
+
+        boxes = relative_to_absolute(boxes, tf.shape(image))
+        adjusted_boxes, adjusted_classes = self._filter_and_adjust_labels(crop_box, boxes, classes)
+
+        return cropped_image, adjusted_boxes, adjusted_classes
+
+    def _augment_data(self, image, boxes, classes):
         if self._split == 'val' and not self._augment_val_dataset:
             return image, boxes
         image = image / 255.0
+        if self._random_patch:
+            image, boxes, classes = self._random_patch_fn(image, boxes, classes)
         if self._random_flip_horizonal:
             image, boxes = self._random_flip_horizontal_fn(image, boxes)
         if self._random_brightness:
@@ -67,7 +110,7 @@ class DatasetBuilder:
         if self._random_saturation:
             image = self._random_saturation_fn(image)
         image = image * 255.0
-        return image, boxes
+        return image, boxes, classes
 
     def _parse_example(self, example_proto):
         feature_description = {
@@ -86,9 +129,6 @@ class DatasetBuilder:
         image = tf.io.decode_image(parsed_example['image'], channels=3)
         image = tf.cast(image, dtype=tf.float32)
         image.set_shape([None, None, 3])
-        original_dims = tf.shape(image)
-        image = tf.image.resize(image,
-                                size=[self._input_height, self._input_width])
 
         boxes = tf.stack([
             tf.sparse.to_dense(parsed_example['xmins']),
@@ -96,18 +136,30 @@ class DatasetBuilder:
             tf.sparse.to_dense(parsed_example['xmaxs']),
             tf.sparse.to_dense(parsed_example['ymaxs']),
         ], axis=-1)
-        boxes = rescale_boxes(boxes,
-                              [original_dims[0], original_dims[1]],
-                              [self._input_height, self._input_width])
+
         return image, boxes, classes
 
     def _parse_and_create_label(self, example_proto):
         image, boxes, classes = self._parse_example(example_proto)
-        image, boxes = self._augment_data(image, boxes)
+        original_dims = tf.cast(tf.shape(image), dtype=tf.float32)
+        boxes =  tf.stack([
+            tf.clip_by_value(boxes[..., 0], 0, original_dims[1]),
+            tf.clip_by_value(boxes[..., 1], 0, original_dims[0]),
+            tf.clip_by_value(boxes[..., 2], 0, original_dims[1]),
+            tf.clip_by_value(boxes[..., 3], 0, original_dims[0])
+        ], axis=-1)
         
+        image, boxes, classes = self._augment_data(image, boxes, classes)
+        new_dims = tf.shape(image)
+        image = tf.image.resize(image,
+                                size=[self._input_height, self._input_width])
+        boxes = rescale_boxes(boxes,
+                              [new_dims[0], new_dims[1]],
+                              [self._input_height, self._input_width])
+
         if 'resnet' in self._backbone:
             image = (image - 127.5) / 127.5
-        
+
         boxes_xywh = convert_to_xywh(boxes)
         label = self._label_encoder.encode_sample(boxes_xywh, classes)
         return image, label
